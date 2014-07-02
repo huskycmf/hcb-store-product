1aa7992df5f62016a9d426931c9f0be977fc1d9d
Oleksii Mylotskyi <spalaxinco@gmail.com>
2014-07-02 13:01:05 +0300

    Fixed mistake with incorrect class name.

--- schema/migrations/Version20140613183358_HcbStoreProduct.php
+++ schema/migrations/Version20140613183358_HcbStoreProduct.php
@@ -7,7 +7,7 @@ use Doctrine\DBAL\Migrations\AbstractMigration,
 /**
  * Auto-generated Migration: Please modify to your need!
  */
-class Version20140613183357_HcbStoreProduct extends AbstractMigration
+class Version20140613183358_HcbStoreProduct extends AbstractMigration
 {
     public function up(Schema $schema)
     {
